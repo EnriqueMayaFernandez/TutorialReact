10716cc9b243f059d938924cf916fd5180d2f7c4
EnriqueMayaFernandez <enriquemayatrabajar@gmail.com>
2022-04-22 13:11:37 +0200

    Terminamos video 5 y 6

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { Link, Route } from "wouter";
-import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import Detail from "./pages/Detail";
 import StaticContext from "./context/StaticContext";
 import { GifsContextProvider } from "./context/GifsContext";
+
+const HomePage = React.lazy(() => import("./pages/Home"));
 
 export default function App() {
   return (
@@ -13,18 +14,20 @@ export default function App() {
       value={{ name: "midudev", suscribeteAlCanal: true }}
     >
       <div className="App">
-        <section className="App-content">
-          <Link to="/">
-            <figure className="App-logo">
-              <img alt="Giffy logo" src='/logo.png' />
-            </figure>
-          </Link>
-          <GifsContextProvider>
-            <Route component={Home} path="/" />
-            <Route component={SearchResults} path="/search/:keyword" />
-            <Route component={Detail} path="/gif/:id" />
-          </GifsContextProvider>
-        </section>
+        <Suspense fallback={null}>
+          <section className="App-content">
+            <Link to="/">
+              <figure className="App-logo">
+                <img alt="Giffy logo" src='/logo.png' />
+              </figure>
+            </Link>
+            <GifsContextProvider>
+              <Route component={HomePage} path="/" />
+              <Route component={SearchResults} path="/search/:keyword" />
+              <Route component={Detail} path="/gif/:id" />
+            </GifsContextProvider>
+          </section>
+        </Suspense>
       </div>
     </StaticContext.Provider>
   );
